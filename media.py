8cb437da2957539c0cf6dae6bf45b1c60c474e2e
Dirk <dirk.possardt1@freenet.de>
2017-04-10 22:36:42 +0200

    docstring comment

--- media.py
+++ media.py
@@ -1,9 +1,9 @@
 import webbrowser
 
 class Movie():
-	#class Movie/ instantiate with: Myname.Movie(four attributes)
-	# four attributes: movie title, movie stroyline, poster image and trailer
-	# method: show_trailer()
+	"""class Movie/ instantiate with: Myname.Movie(four attributes)
+	   four attributes: movie title, movie stroyline, poster image and trailer
+	   method: show_trailer()"""
 	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
 		self.title = movie_title
 		self.storyline = movie_storyline
@@ -11,7 +11,7 @@ class Movie():
 		self.trailer_youtube_url = trailer_youtube
 
 	def show_trailer(self):
-		# show trailer method of class Movie
-		# opens the browser with the given url
-		# input: trailer_youtube_url [string]
+		""" show trailer method of class Movie
+		    opens the browser with the given url
+		    input: trailer_youtube_url [string] """
 		webbrowser.open(self.trailer_youtube_url)
